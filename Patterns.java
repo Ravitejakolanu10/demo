111e491df65556fb0c96f3c5efea02615438960c
ravi teja <ravitejakolanu10@gmail.com>
2024-08-04 00:06:34 +0530

    Happy

--- Patterns.java
+++ Patterns.java
@@ -25,6 +25,26 @@ public class Patterns{
 
         System.out.println();
 
+        for(int i=1;i<=10;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {   
+                if(j==1 || i==10)
+                {
+                    System.out.print("**");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+                if(i==10)
+                {
+                    System.out.print("*");
+                }
+            } 
+            System.out.println();
+        }
+
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=2*(n-i);j++)
@@ -48,6 +68,30 @@ public class Patterns{
             }
             System.out.println();
         }
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=2*(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(" ");
+            }for(int j=1;j<=i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(i);j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        
 
     }
 }
