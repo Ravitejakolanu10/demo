7d42b9f23924842c27c221e8ad4d16f2fed2526c
ravi teja <ravitejakolanu10@gmail.com>
2024-08-03 23:03:00 +0530

    right angle traingle

--- Patterns.java
+++ Patterns.java
@@ -10,5 +10,17 @@ public class Patterns{
             }
             System.out.println();
         }
+
+
+        System.out.println();
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 }
